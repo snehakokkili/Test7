5cc52cd0ed53d789ca1060142a8f5762d8de377a
Sneha <snehakokkiligadda9@gmail.com>
2023-10-15 00:17:58 +0530

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,62 @@
+const header = {
+    homepage: 'https://snehakokkili.github.io/cleanfolio',
+    title: 'Hello!',
+  }
+  
+  const about = {
+    name: 'Sneha',
+    role: 'Computer Science Engineer',
+    description:
+      'Currently pursuing my B.tech 3rd year at VIT AP University.',
+    resume: 'https://example.com',
+    social: {
+      linkedin: 'https://www.linkedin.com/in/sneha-kokkiligadda-228144255',
+      github: 'https://github.com/snehakokkili',
+    },
+  }
+  
+  const projects = [
+   
+    {
+      name: 'Project 1',
+      description:
+        'Stock Price Prediction Using Recurrent Neural Networks (RNN) ',
+      stack: ['ML', 'Rnn', 'React'],
+      sourceCode: 'https://github.com',
+      livePreview: 'https://github.com',
+    },
+    {
+      name: 'Project 2',
+      description:
+        'Smart System for Road Safety and Emergency Response',
+      stack: ['Arduino', 'c++', 'React'],
+      sourceCode: 'https://github.com',
+      livePreview: 'https://github.com',
+    },
+    {
+      name: 'Project 3',
+      description:
+        'Reasearch Paper on Criminal Investigation and Privacy in India.',
+      stack: ['SASS', 'TypeScript', 'React'],
+      sourceCode: 'https://github.com',
+      livePreview: 'https://github.com',
+    },
+  ]
+  
+  const skills = [
+    
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Git',
+    'Sql',
+    'Java',
+    'Python'
+  ]
+  
+  const contact = {
+    email: 'narendradasu3@mail.com',
+  }
+  
+  export { header, about, projects, skills, contact }
